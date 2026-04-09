5762a05e9b5e930ec4b3e822329e6934868c0e4a
Nicolas Rocha <111698649+nik444ds@users.noreply.github.com>
2026-04-09 21:17:56 +0100

    dataStructure Library

---
+++ src/youVideo/Main.java
@@ -0,0 +1,20 @@
+ import youVideo.*;
+import dataStructures.*;
+
+public class Main {
+
+}
+
+
+/*
+Classes and Interfaces
+VideoStructure - A abstract class which implements: ID, Duration and URL.
+PublishableVideos - A class which extend VideoStructure and implements a publisher, title and languages for a video .
+PremiumVideos - A class which extends the PublishableVideos and have a subtitle implemented early
+Podcasts - A class which implements a title, author and language.
+Episodes - A class which extends VideoStructure
+Shows -A
+IteratorVideo Interface
+IteratorVideo Class
+
+ */
