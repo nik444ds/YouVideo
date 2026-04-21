235e932b72367a98651300f5f547934a3b78c4cd
Nicolas Rocha <111698649+nik444ds@users.noreply.github.com>
2026-04-21 17:56:32 +0100

    Episode and Podcastclass

--- src/youVideo/Podcasts.java
+++ src/youVideo/Podcasts.java
@@ -1,4 +1,30 @@
 package youVideo;
 
-public class Podcasts {
+import dataStructures.Array;
+
+import java.util.Locale;
+
+public class Podcasts  {
+private String title;
+private String author;
+private Locale language;
+private Array<Episode> episode;
+
+    public Podcasts( String title, String author, String language){
+        this.title = title;
+        this.author = author;
+        this.language = new Locale (language);
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public Locale getLanguage(){
+        return language;
+    }
+    public Array<Episode> getEpisode(){
+        return episode;
+    }
 }
