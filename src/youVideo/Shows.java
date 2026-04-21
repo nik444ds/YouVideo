41721cf5a4f1ba02e9f6b34031b9c5b0f9c775b8
Mahir <mahirmomin2006@gmail.com>
2026-04-21 17:55:28 +0100

    Update Shows.java
    did shows class

--- src/youVideo/Shows.java
+++ src/youVideo/Shows.java
@@ -1,4 +1,28 @@
 package youVideo;
 
 public class Shows {
+    private String author;
+    private PublishableVideos video;
+    private String date;
+
+    public Shows(String author, PublishableVideos video, String date) {
+        this.author = author;
+        this.video = video;
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public PublishableVideos getvideo() {
+        return video;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public String getTitle(){
+        return video.getTitle();
+    }
 }
